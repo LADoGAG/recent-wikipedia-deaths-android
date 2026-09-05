965e22a1eb8bc9b8a8173082ada3147851a28959
Ladoga Games <aakolobaev@gmail.com>
2026-09-05 17:32:12 +0300

    chore: bump version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,8 +24,8 @@ android {
         applicationId = "io.github.ladogag.recentwikipediadeaths"
         minSdk = 24
         targetSdk = 37
-        versionCode = 4
-        versionName = "1.0.3"
+        versionCode = 5
+        versionName = "1.0.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
